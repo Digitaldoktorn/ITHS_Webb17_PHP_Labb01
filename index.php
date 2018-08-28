a3e19e23635b16e731f08d1d96c28c699b5e00d2
Anders Eriksson <anderse@unseen.is>
2018-08-28 21:48:47 +0200

    commenting the structure of the program

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
     echo "<h1>Labb 1 - Anders Eriksson</h1>";
+
+    // get data from csv file into array
+    $csv_data = array_map('str_getcsv', file("international-orders.csv"));
+    echo "<pre>";
+    var_dump($csv_data);
+    echo "</pre>";
+
+    $id;
+    // get id
+
+    
+    // calculate the total sum of prices from a chosen country
+    function calculateTotalSum($id) {
+        if($id){
+            // get price
+            // loop price and get sum
+            // print data to csv file - Success, SE, total sum
+        }
+
+    }
+
+    // call the function
+    calculateTotalSum("#SE");
 ?>
