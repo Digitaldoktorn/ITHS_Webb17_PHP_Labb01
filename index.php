c3f8448514e4507f8c1117eb2889767c45bc8382
Anders Eriksson <anderse@unseen.is>
2018-08-29 13:35:25 +0200

    for loop to get 9 char ID and get country code

--- index.php
+++ index.php
@@ -1,26 +1,45 @@
 <?php
     echo "<h1>Labb 1 - Anders Eriksson</h1>";
 
-    // get data from csv file into array
+    // Get data from csv file into array
     $csv_data = array_map('str_getcsv', file("international-orders.csv"));
     echo "<pre>";
     var_dump($csv_data);
     echo "</pre>";
+    echo "<br>";
 
-    $id;
-    // get id
+    // Testar bara array values
+    // print_r(array_values($csv_data));
 
-    
-    // calculate the total sum of prices from a chosen country
-    function calculateTotalSum($id) {
+    // Loop through the array to get values etc
+    for($i=0; $i<sizeof($csv_data); $i++) {
+        // Find IDs with 9 characters
+        if(strlen($csv_data[$i][0]) == 9){
+            // echo $i . ": " . "Success\n" . "<br>";
+
+            // Get the full ID string into variable
+            $id = $csv_data[$i][0];
+
+            // Get country code from ID into variable
+            $countryCode = substr($id, 0, 3);
+        }
+        else {
+            echo $i . ": " . "Failure\n" . "<br>";
+        }
+    }
+    echo "<br>";
+
+
+
+    // Calculate the total sum of prices from a chosen country
+    function calculateTotalSum($countryCode) {
         if($id){
             // get price
             // loop price and get sum
             // print data to csv file - Success, SE, total sum
         }
-
     }
 
-    // call the function
+    // Call the function
     calculateTotalSum("#SE");
 ?>
