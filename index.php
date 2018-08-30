e459deb39663f4b8f47ae292126897fc4e0770a1
Anders Eriksson <anderse@unseen.is>
2018-08-30 17:15:59 +0200

    extracted total sum from #FI

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
+    // Se alla fel under development.
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     echo "<h1>Labb 1 - Anders Eriksson</h1>";
 
     // Get data from csv file into array
     $csv_data = array_map('str_getcsv', file("international-orders.csv"));
-    echo "<pre>";
-    var_dump($csv_data);
-    echo "</pre>";
-    echo "<br>";
+    // echo "<pre>";
+    // var_dump($csv_data);
+    // echo "</pre>";
+
+    $price = 0;
+    $quantity = 0;
+    $status = 0;
+    $id;
+    $countryCode;
+
+    $totalSumFi = 0;
+
+
+
+    echo "<br>---------------------------------------------------------------</br>";
 
     // Loop through the array to get values etc
     for($i=0; $i<sizeof($csv_data); $i++) {
@@ -30,39 +46,51 @@
             echo "Valid country code: " . $countryCode . "<br>";
             echo "Quantity: " . $quantity . "<br>";
             echo "Price: " . $price . "<br>";
+            echo "-------------------------</br>";
+            if($countryCode === "#FI"){
+                $totalSumFi += $price * $quantity;
+            }
+    
+            else {
+                // echo "Failure! You have entered an invalid country code. Please try again.";
+            }
+            
+            
         }
         else {
             // echo $i . ": " . "Not a valid country code. \n" . "<br>";
-        }
-        if($countryCode === "#FI"){
-            echo "Finland q:" . $quantity;
-        }
-        $arrayCountryCodes[] = $countryCode;
-        foreach($arrayCountryCodes as $cCode){
-            $listCountryCodes = $cCode;
         } 
-        
     }
-    echo "<br>";
-    echo $listCountryCodes;
-   
+    echo "Total sum Finland: " . $totalSumFi . ":-<br>";
+
+    echo "<br>-------------------------------------------------------------------</br>";
+    // Loop to get country codes without duplicates - funkar ej
+    /*
+    for($i=0; $i<sizeof($csv_data); $i++) {
+        $id = $csv_data[$i][0];
+        $countryCode = substr($id, 0, 3);
+        $countryCodes = str_split($countryCode, 3);
+        print_r(array_unique($countryCodes));
+    }
+    */
 
     // Present valid country codes to the user
-    // echo "Valid country codes are: #SE, #FI, #US, #DE, #ES, #RU, #NO, #IT, #GR, #FR, #PL<br>";
+    echo "Valid country codes are: #SE, #FI, #US, #DE, #ES, #RU, #NO, #IT, #GR, #FR, #PL<br>";
 
-    $status = 0;
+   
 
     // Calculate the total sum of prices from a chosen country
-    function calculateCountrysTotalSum($countryCode) {
-        if($countryCode === "#FI"){
-            
-        }
-
-        else {
-            echo "Failure! You have entered an invalid country code. Please try again.";
-        }
-    }
+    // function calculateCountrysTotalSum($countryCode) {
+        
+            // if($countryCode === "#FI"){
+            //     $priceSumFi += $price;
+            // }
+    
+            // else {
+            //     echo "Failure! You have entered an invalid country code. Please try again.";
+            // }
+    // }
 
     // Call the function
-    calculateCountrysTotalSum("#FI");
+    // calculateCountrysTotalSum("#FI");
 ?>
